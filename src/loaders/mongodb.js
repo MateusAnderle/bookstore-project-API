870527f71faa6a74ac2964a2277a2498f7ba3bae
Mateus Anderle da Silva <matsanderle@gmail.com>
2023-01-16 15:06:17 -0300

    Bug fix

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function startDB() {
-    await mongoose.connect('mongodb+srv://mateusanderle:a1b2c3d4e5@cluster0.b6m7qh4.mongodb.net/test');
+    await mongoose.connect('');
 }
 
 module.exports = startDB;
