597c3875813f0aceea9a520ecf98daf913e88826
Mateus Anderle da Silva <matsanderle@gmail.com>
2023-01-17 11:41:58 -0300

    Cors updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 require('dotenv').config()
 const app = require('./app');
 const Loaders = require('./loaders');
-const cors = require('cors');
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*")
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-    app.use(cors());
-    next();
-})
 
 Loaders.start();
 
