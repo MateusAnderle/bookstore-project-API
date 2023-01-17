bd87cafc090ffaac8919a664f6857d4df2f5d84f
Mateus Anderle da Silva <matsanderle@gmail.com>
2023-01-17 11:27:07 -0300

    Allow access control

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const Loaders = require('./loaders');
 const cors = require('cors');
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000")
     app.use(cors());
     next();
 })
