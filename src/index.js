11fc2b74be1072b912ee7181244eea9c91bce93d
Mateus Anderle da Silva <matsanderle@gmail.com>
2023-01-17 10:18:54 -0300

    Cors added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const app = require('./app');
 const Loaders = require('./loaders');
-require('dotenv').config()
+const cors = require('cors');
 
 Loaders.start();
 
+app.use(cors());
 app.listen(3333, () => console.log('Server is running'));
