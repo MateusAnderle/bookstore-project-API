540b64173b665960588127f7f7234838db6a6d5d
Mateus Anderle da Silva <matsanderle@gmail.com>
2023-01-17 11:33:50 -0300

    Change header

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const Loaders = require('./loaders');
 const cors = require('cors');
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "http://localhost:3000")
+    res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
     app.use(cors());
     next();
 })
